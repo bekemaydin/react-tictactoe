e47f6ba7ede611e5162e2a81e5cba7ee2a0154b0
Mehmet Aydın Bekem <bekemaydin@gmail.com>
2020-12-28 15:25:22 +0300

    'some css codes changed'

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -90,7 +90,7 @@ function Square(props) {
               'Go to game start';
             return (
               <li key={move}>
-                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                <button id='jumpButton' onClick={() => this.jumpTo(move)}>{desc}</button>
               </li>
             );
           });
